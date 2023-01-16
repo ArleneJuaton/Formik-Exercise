2e301aece23801916b390cb4a145bc1bee21c1a1
Arlene Juaton <113062231+ArleneJuaton@users.noreply.github.com>
2023-01-16 13:31:48 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React from "react"
+import { useFormik } from "formik";
+
+
+function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (values, {resetForm}) => {
+      console.log('form:', values);
+      alert(`Login Successful! \nWelcome ${values.name}`);
+      resetForm({ values: ""});
+    },
+    validate: values => {
+      let errors = {};
+      if(!values.email) errors.email = "Field Required";
+      if(!values.password) errors.password = "Field Required";
+      return errors;
+    }
+  });
+
+  return (
+    <div className="app d-flex align-items-center justify-content-center mt-5">
+      <div className="d-flex flex-column border rounded-3 p-3 shadow" style={{ width: "25rem" }}>
+        <h2 className="text-center">Login</h2>
+        <form onSubmit={formik.handleSubmit}>
+          <div>Name</div>
+          <input
+            id="nameField"
+            name="name"
+            type="text"
+            className="form-control"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
+
+          <div>Email</div>
+          <input
+            id="emailField"
+            name="email"
+            type="text"
+            className="form-control"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          {formik.errors.email ? (
+            <div
+              id="emailError"
+              style={{ color: "red"}}
+            >
+              {formik.errors.email}
+            </div>
+          ) : null}
+
+          <div>Password</div>
+          <input
+            id="pswField"
+            name="password"
+            type="password"
+            className="form-control"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+          {formik.errors.password ? (
+            <div
+              id="pswError"
+              style={{ color: "red"}}
+            >
+              {formik.errors.password}
+            </div>
+          ) : null}
+
+        <div>
+          <button
+            id="submitBtn"
+            className="form-control btn btn-primary btn-block mt-3"
+            type="submit"
+          >Submit</button>
+        </div>
+      </form>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/formik-ex.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import BasicForm from "./components/BasicForm";
+import "./index.css";
+
+function App() {
+    const [view, setView] = useState("basic");
+    return (
+        <div className="App">
+            
+        </div>
+    )
+}
